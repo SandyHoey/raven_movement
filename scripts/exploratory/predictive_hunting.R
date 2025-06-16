ed3cfdb642433b50c0b8a257d4a436660276eab0
Cameron Ho <ho.cameron98@gmail.com>
2025-06-16 14:07:31 -0600

    removed roosts. Checking cluster validity

--- scripts/exploratory/predictive_hunting.R
+++ scripts/exploratory/predictive_hunting.R
@@ -20,14 +20,14 @@ library(lubridate)
 allGPS <- readr::read_csv("data/clean/all_raven_gps_clean28.csv") %>% 
   
   #removing rows with NA coords
-  filter(!is.na(utm.easting)) %>% 
+  filter(!is.na(utm_easting)) %>% 
   
   #filter to only winter 
-  filter(month(study.local.timestamp) %in% 10:12)
+  filter(month(study_local_timestamp) %in% 11:12)
 
 # #removing 7653 and 7596 because Canada
-# allGPS <- subset(allGPS, individual.local.identifier != "7653")
-# allGPS <- subset(allGPS, individual.local.identifier != "7596")
+# allGPS <- subset(allGPS, individual_local_identifier != "7653")
+# allGPS <- subset(allGPS, individual_local_identifier != "7596")
 
 
 # #importing demographic information
@@ -39,25 +39,25 @@ allGPS <- readr::read_csv("data/clean/all_raven_gps_clean28.csv") %>%
 # 
 # 
 # #subsetting territorials from entire GPS df
-# nbGPS <- subset(allGPS, individual.local.identifier %in% nb)
+# nbGPS <- subset(allGPS, individual_local_identifier %in% nb)
 # 
 # 
 # #subsetting trans territorials into their active territorial periods from entire GPS df
-# transGPS <- allGPS[allGPS$individual.local.identifier %in% trans$`tag-id`,]
+# transGPS <- allGPS[allGPS$individual_local_identifier %in% trans$`tag-id`,]
 # 
-# transGPS <- do.call("rbind", tapply(transGPS, INDEX=transGPS$individual.local.identifier, 
+# transGPS <- do.call("rbind", tapply(transGPS, INDEX=transGPS$individual_local_identifier, 
 #                                     FUN=function(x){
-#                                       ind <- trans[trans$`tag-id` == x[1,]$individual.local.identifier,]
+#                                       ind <- trans[trans$`tag-id` == x[1,]$individual_local_identifier,]
 #                                       
 #                                       #has only an end date
 #                                       if(is.na(ind$`start date`) & !is.na(ind$`leave date`)){
-#                                         tmp <- x[as.Date(x$study.local.timestamp) > lubridate::ym(ind$`leave date`),]
+#                                         tmp <- x[as.Date(x$study_local_timestamp) > lubridate::ym(ind$`leave date`),]
 #                                         return(tmp)
 #                                       }
 #                                       
 #                                       #has only a start date
 #                                       if(!is.na(ind$`start date`) & is.na(ind$`leave date`)){
-#                                         tmp <- x[as.Date(x$study.local.timestamp) < lubridate::ym(ind$`start date`),]
+#                                         tmp <- x[as.Date(x$study_local_timestamp) < lubridate::ym(ind$`start date`),]
 #                                         return(tmp)
 #                                       }
 #                                       
@@ -78,7 +78,7 @@ allGPS <- readr::read_csv("data/clean/all_raven_gps_clean28.csv") %>%
 
 # #transforming raven points to sf dataframe
 # #allows a distance metric to the polygon to be calculated
-# sf_raven <- st_as_sf(gps_w, coords=c("utm.easting", "utm.northing"),
+# sf_raven <- st_as_sf(gps_w, coords=c("utm_easting", "utm_northing"),
 #                       crs="+proj=utm +zone=12")
 # 
 # 
@@ -91,16 +91,16 @@ allGPS <- readr::read_csv("data/clean/all_raven_gps_clean28.csv") %>%
 cluster_raw <- allGPS %>% 
   
   #pulling out only necessary data for clustering
-  dplyr::select(study.local.timestamp, location.lat, location.long, individual.local.identifier) %>% 
-  mutate(AID = paste0(individual.local.identifier, "_", as.Date(study.local.timestamp)),
-         study.local.timestamp = force_tz(study.local.timestamp, tz = "MST")) %>% 
-  rename(TelemDate = study.local.timestamp,
-         Long = location.long,
-         Lat = location.lat) %>%
+  dplyr::select(study_local_timestamp, location_lat, location_long, individual_local_identifier) %>% 
+  mutate(AID = paste0(individual_local_identifier, "_", as.Date(study_local_timestamp)),
+         study_local_timestamp = force_tz(study_local_timestamp, tz = "MST")) %>% 
+  rename(TelemDate = study_local_timestamp,
+         Long = location_long,
+         Lat = location_lat) %>%
   
   #remove days that don't have 4 points in Gardiner (the min for a GPS cluster)
   group_by(AID) %>%
-  filter(n() >= 4) %>% 
+  filter(n() >= 2) %>% 
   as.data.frame %>% 
   
   arrange(AID)
@@ -152,7 +152,7 @@ cluster_output <- GPSeq_clus(cluster_raw,
                        search_radius_m = 50,
                        window_days = 1,
                        clus_min_locs = 4,
-                       show_plots = F)
+                       show_plots = c(F, "mean"))
 
 #pulling out clusters
 clusters <- cluster_output[[2]]
@@ -230,20 +230,31 @@ combine_cluster <- function(gps_sf, datetime_col = "clus_start",
   return(gps_sf)
 }
 
-cluster_group <- combine_cluster(cluster_hunt)
+cluster_roost <- combine_cluster(cluster_hunt) %>% 
+
+#removing clusters that are probably roosts
+#looking at number of night points
+#night is defined in the cluster algorithm as suncalc() sunrise/sunset
+  filter(night_prop < .2) %>% 
+  
+  #only using days in early winter study 11-15 to 12-15
+  filter(clus_end >= as.Date(paste(year(clus_end), 11, 15, sep = "-")),
+         clus_start <= as.Date(paste(year(clus_start), 12, 15, sep = "-")))
+
 
 #creating a new dataframe with the start and stop times for each cluster
-cluster_meta <- data.frame(group_id = unique(cluster_group$group_id))
+cluster_meta <- data.frame(group_id = unique(cluster_roost$group_id))
 
 
-cluster_final <- cluster_group %>% 
+cluster_final <- cluster_roost %>% 
   st_drop_geometry %>% 
   group_by(group_id) %>% 
   summarise(start_date = as.Date(min(clus_start)),
             end_date = as.Date(max(clus_start)),
             easting = mean(easting),
             northing = mean(northing)) %>% 
-  right_join(cluster_meta, by = join_by(group_id))
+  right_join(cluster_meta, by = join_by(group_id)) %>% 
+  st_as_sf(coords = c("easting", "northing"), crs = "+proj=utm +zone=12")
 
 
 # #plotting   
@@ -252,25 +263,45 @@ cluster_final <- cluster_group %>%
 #   mutate(group_id = as.factor(group_id))
 # 
 # # Plot on satellite map
-# mapview::mapview(gps_data, zcol = "group_id", map.type = "Esri.WorldImagery", legend = TRUE)
+mapview::mapview(cluster_final %>% filter(year(start_date) == 2024), map.type = "Esri.WorldImagery")
 
+#how many clusters per year
+table(year(cluster_final$start_date))
+
+#how many individuals giving GPS per year
+a <- cluster_raw %>% 
+  filter(year(TelemDate) > 2018) %>% 
+  group_by(year(TelemDate)) %>% 
+  summarise(ind_per_year = length(unique(individual_local_identifier)))
+
+#number of ravens that visited each cluster
+b <- cluster_roost %>% 
+  mutate(ID = sub("(_\\d{4}-\\d{2}-\\d{2})$", "", AID)) %>% 
+  st_drop_geometry %>% 
+  group_by(group_id) %>% 
+  summarise(ind = length(unique(ID)), year = mean(year(clus_start))) %>% 
+  group_by(year) %>% 
+  summarise(mean(ind))
+
+#plotting "number of tagged individuals" vs "average individuals per cluster"
+plot(x = a$ind_per_year, y = b$`mean(ind)`)
 # HMM ---------------------------------------------------------------------
 # 
 # #prepping data for HMM package
 # input_data <- gps_final %>% 
 #   
 #   #creating a ID column moveHMM can recognize
-#   mutate(ID = paste0(individual.local.identifier, "_", as.Date(study.local.timestamp))) %>%
+#   mutate(ID = paste0(individual_local_identifier, "_", as.Date(study_local_timestamp))) %>%
 #   
 #   #selecting only relevant columns
-#   dplyr::select(ID, utm.easting, utm.northing) %>% 
+#   dplyr::select(ID, utm_easting, utm_northing) %>% 
 #   
 #   #removing days for individuals when there are less than 3 GPS points
 #   group_by(ID) %>% 
 #   filter(n() > 3) %>% 
 #   
 #   #prepping data with moveHMM function
-#   prepData(type = "UTM", coordNames = c("utm.easting", "utm.northing"))
+#   prepData(type = "UTM", coordNames = c("utm_easting", "utm_northing"))
 # 
 # 
 # 
