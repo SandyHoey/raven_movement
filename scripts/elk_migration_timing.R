70b58a3c5de0bf1f4251fdcd9dabc1fb29f34628
Cameron Ho <ho.cameron98@gmail.com>
2025-10-07 16:01:58 -0600

    added full hunting season

--- scripts/elk_migration_timing.R
+++ scripts/elk_migration_timing.R
@@ -62,104 +62,137 @@ elk_ynp <- elk_data %>%
 
 
 # number of collared elk each month in Jardine ---------------------------
-monthly_count <- elk_ynp %>%
+# monthly_count <- elk_ynp %>%
+# 
+#   
+#   #adding year and month columns
+#   mutate(year = year(dt),
+#          month = month(dt)) %>%
+#   group_by(year, month) %>%
+#   
+#   #summarize data
+#   summarise(outside_park = n_distinct(ID), .groups = "drop")
+# 
+# 
+# #adding the total sample size each month
+# monthly_count <- elk_data %>% 
+#   
+#   #removing sf geometry
+#   st_drop_geometry() %>% 
+#   
+#   #adding year and month columns
+#   mutate(year = year(dt),
+#          month = month(dt)) %>%
+#   group_by(year, month) %>%
+#   
+#   #summarize data
+#   summarise(total_ind = n_distinct(ID), .groups = "drop") %>% 
+#   
+#   #merging with monthly count
+#   right_join(monthly_count, by = c("year" = "year", "month" = "month")) %>% 
+#   
+#   #adding column with proportion
+#   mutate(prop_available = outside_park/total_ind) %>% 
+#   
+#   #adding column with combined year-month
+#   mutate(ym = paste(year, month, sep="-"))
+# 
+# 
+# #average by month
+# monthly_count %>% 
+#   group_by(month) %>% 
+#   summarise(mean(prop_available))
+# boxplot(prop_available ~ month, data = monthly_count)
+# 
+# #plotting 
+# boxplot(prop_available ~ ym, data = monthly_count)
 
+
+# number of collared elk daily during hunting season in Jardine ---------------------------
+
+#reading in hunting season start and end dates
+hunting_dates <- readxl::read_xlsx("data/raw/hunting_seasons.xlsx")
+
+
+daily_count <- elk_ynp %>%
   
-  #adding year and month columns
+  #adding year, month, and day columns
   mutate(year = year(dt),
-         month = month(dt)) %>%
-  group_by(year, month) %>%
+         month = month(dt),
+         day = day(dt),
+         date = as.Date(dt)) %>%
+  
+  #adding hunting season dates
+  left_join(hunting_dates %>% 
+              dplyr::select(year, start, end),
+            by = join_by(year)) %>% 
+  
+  #filtering to only hunting season
+    #creating new boolean column for hunting season
+    mutate(hunt = if_else((format(date, "%m-%d") >= 
+                            format(start, "%m-%d")) &
+                          (format(date, "%m-%d") <= 
+                             format(end, "%m-%d")), 
+                          1, #days in nov before end date
+                          0)) %>%  #otherwise no hunting == 0 
+    filter(hunt == 1) %>% 
   
   #summarize data
+  group_by(year, month, day) %>%
   summarise(outside_park = n_distinct(ID), .groups = "drop")
 
-
-#adding the total sample size each month
-monthly_count <- elk_data %>% 
   
-  #removing sf geometry
-  st_drop_geometry() %>% 
-  
-  #adding year and month columns
-  mutate(year = year(dt),
-         month = month(dt)) %>%
-  group_by(year, month) %>%
-  
-  #summarize data
-  summarise(total_ind = n_distinct(ID), .groups = "drop") %>% 
-  
-  #merging with monthly count
-  right_join(monthly_count, by = c("year" = "year", "month" = "month")) %>% 
-  
-  #adding column with proportion
-  mutate(prop_available = outside_park/total_ind) %>% 
-  
-  #adding column with combined year-month
-  mutate(ym = paste(year, month, sep="-"))
-
-
-#average by month
-monthly_count %>% 
-  group_by(month) %>% 
-  summarise(mean(prop_available))
-boxplot(prop_available ~ month, data = monthly_count)
-
-#plotting 
-boxplot(prop_available ~ ym, data = monthly_count)
-
-
-# number of collared elk daily during November in Jardine ---------------------------
-nov_count <- elk_ynp %>%
-  
-  #only november
-  filter(month(dt) == 11) %>% 
-  
-  #adding year and month columns
-  mutate(year = year(dt),
-         day = day(dt)) %>%
-  group_by(year, day) %>%
-  
-  #summarize data
-  summarise(outside_park = n_distinct(ID), .groups = "drop")
+#fixing an issue with the early hunting season of 2024 not having any data
+#since no elk were out of the park
+daily_count <- data.frame(year = 2024,
+           month = 10,
+           day = c(26:29),
+           outside_park = 0) %>% 
+  bind_rows(daily_count) %>% 
+  arrange(year, month, day)
 
 
 #adding the total sample size every day
-nov_count <- elk_data %>% 
+daily_count <- elk_data %>% 
   
   #removing sf geometry
   st_drop_geometry() %>% 
   
-  #only november
-  filter(month(dt) == 11) %>% 
+  #only hunting months
+  filter(month(dt) %in% c(10, 11, 12)) %>% 
   
   #adding year and month columns
   mutate(year = year(dt),
+         month = month(dt),
          day = day(dt)) %>%
-  group_by(year, day) %>%
+  group_by(year, month, day) %>%
   
   #summarize data
   summarise(total_ind = n_distinct(ID), .groups = "drop") %>% 
   
   #merging with monthly count
-  right_join(nov_count, by = c("year" = "year", "day" = "day")) %>% 
+  right_join(daily_count, by = join_by(year, month, day)) %>% 
   
-  #adding column with proportion
-  mutate(prop_available = outside_park/total_ind) %>% 
-  
-  #adding column with combined year-month
-  mutate(yd = paste(year, day, sep="-"))
+  #adding columns 
+  mutate(
+    #with proportion
+    prop_available = outside_park/total_ind,
+    
+    #adding month-day
+    md = paste(month, day, sep = "-")) 
+
 
 
 #average by day
-nov_count %>% 
-  group_by(day) %>% 
-  summarise(mean(prop_available))
-boxplot(prop_available ~ day, data = nov_count)
+# daily_count %>% 
+#   group_by(md) %>% 
+#   summarise(mean(prop_available))
+# boxplot(prop_available ~ md, data = daily_count)
 
 
 #plotting splitting years
-nov_count %>% 
-  ggplot(aes(x = day, y = prop_available, 
+daily_count %>% 
+  ggplot(aes(x = as.Date(paste(2000, md, sep = "-")), y = prop_available, 
              group = year, col = factor(year))) +
   geom_line()
 
