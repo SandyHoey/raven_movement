8583add1c8df1f218604e6b5a06bf8cf7b262660
Cameron Ho <ho.cameron98@gmail.com>
2025-10-22 13:55:42 -0600

    changed to output data so instead of sourcing for model script

--- scripts/binomial model.R
+++ scripts/binomial model.R
@@ -1,4 +1,19 @@
 #modeling the impacts of food availability on raven winter movements decisions
 
-source("scripts/covariates.R")
+library(lme4)
+library(DHARMa)
+
+
+#3 = has a least 1 point that day in Gardiner poly
+#2 = has at least 1 point farther than 1 km from terr poly , but none in Gardiner poly
+#1 = only has points in terr poly
+
+
+#restricting to only winter study periods
+model_data <- readr::read_csv("data/clean/commute_data.csv") %>% 
+  
+  #restricting to only winter study months
+  filter(month %in% c(11, 12, 3),
+         !(month %in% c(11, 12) & is.na(bms_window_1)))
+
 
