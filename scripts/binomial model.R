ee24fbe1e42415ee0b70ee91010f32510b146164
Cameron Ho <ho.cameron98@gmail.com>
2025-10-23 10:36:29 -0600

    added conditional binomial model structure

--- scripts/binomial model.R
+++ scripts/binomial model.R
@@ -1,19 +1,70 @@
 #modeling the impacts of food availability on raven winter movements decisions
 
+library(dplyr)
 library(lme4)
 library(DHARMa)
 
 
-#3 = has a least 1 point that day in Gardiner poly
-#2 = has at least 1 point farther than 1 km from terr poly , but none in Gardiner poly
-#1 = only has points in terr poly
-
-
 #restricting to only winter study periods
-model_data <- readr::read_csv("data/clean/commute_data.csv") %>% 
+full_model_data <- readr::read_csv("data/clean/commute_data.csv") %>% 
   
   #restricting to only winter study months
   filter(month %in% c(11, 12, 3),
          !(month %in% c(11, 12) & is.na(bms_window_1)))
 
 
+# checking correlation between biomass covariates --------------------
+# cor.test(full_model_data$bms_window_1, full_model_data$bms_window_3)
+# cor.test(full_model_data$bms_window_1, full_model_data$bms_window_5)
+# cor.test(full_model_data$bms_window_5, full_model_data$bms_window_3)
+# #moving averages are basically the same
+# 
+# #non-average options are still very similar
+# cor.test(full_model_data$bms_window_1, full_model_data$bison_daily_bms)
+# cor.test(full_model_data$bms_window_1, full_model_data$bison_daily_take)
+
+
+# model setup -------------------------------------------------------------
+
+#optimizer for glmer
+cntrl <- glmerControl(optimizer = "bobyqa", tol = 1e-4, optCtrl=list(maxfun=100000))
+
+
+# part 1 of conditional model (stay/leave territory) ---------------------------------------------
+# modeling first part of conditional binomial model
+# did the raven choose to leaves its territory
+
+## DEPENDENT VARIABLES ##
+# terr_bin
+# 1 = left territory
+# 0 = stayed on territory
+
+#comparing models with different biomass window sizes
+mod_terr_bms3 <- glmer(terr_bin ~ (1|raven_id) + active_kill * scale(bms_window_3) + scale(yearly_terr_kill_density) + 
+                         scale(dist2nentrance) + study_period + weekend,
+                       data = full_model_data,
+                       family = "binomial",
+                       control = cntrl)
+
+summary(mod_terr_bms3)
+
+
+# PART 2 of conditional model (visit gardiner/other) ----------------------
+#modeling second part of conditional binomial model
+# if the raven chose to leave its territory, did it visit the hunting area or not
+
+## DEPENDENT VARIABLES ##
+# hunt_bin
+# 1 = visited hunting
+# 0 = visited other place
+
+#creating new data frame to only have days ravens decided to leave territory
+leave_model_data <- full_model_data %>% 
+  filter(terr_bin == 1)
+
+mod_hunt_bms3 <- glmer(hunt_bin ~ (1|raven_id) + scale(bms_window_3) + scale(dist2nentrance) + study_period + weekend,
+                       data = leave_model_data,
+                       family = "binomial",
+                       control = cntrl)
+
+summary(mod_hunt_bms3)
