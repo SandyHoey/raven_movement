46c7db76da9bedea72b4b241fde7ca938d35fc23
Cameron Ho <ho.cameron98@gmail.com>
2025-11-10 12:04:50 -0700

    wrote out data to map raven GPs based on model covariates

--- scripts/exploratory/subsetting_mapping_datasets.R
+++ scripts/exploratory/subsetting_mapping_datasets.R
@@ -10,11 +10,11 @@ library(lubridate)
 # used to identify the hunting area used by ravens 
 
 #reading in raven GPS data
-raven_gps <- read_csv("data/clean/all_raven_gps_clean29.csv") %>% 
+raven_gps_sf <- read_csv("data/clean/all_raven_gps_clean29.csv") %>% 
   clean_names() %>% 
   #selecting useful columns
   dplyr::select(individual_local_identifier, utm_easting, utm_northing, study_local_timestamp) %>%
-  #only complete columns
+  #only complete rows
   na.omit %>% 
   #transforming to sf object
   st_as_sf(coords=c("utm_easting", "utm_northing"), 
@@ -22,15 +22,15 @@ raven_gps <- read_csv("data/clean/all_raven_gps_clean29.csv") %>%
 
 #reading in Yellowstone polygon
 park_poly <- st_read("data/raw/parkpoly.kml") %>% 
-  st_transform(crs = st_crs(raven_gps))
+  st_transform(crs = st_crs(raven_gps_sf))
 
 #only GPS points outside of YNP during November
-raven_gps_outside_ynp <- raven_gps %>% 
+raven_gps_outside_ynp <- raven_gps_sf %>% 
   #calculate distance to Yellowstone (0 == inside park)
   st_distance(park_poly) %>% 
   as.vector %>% 
   #adding back to gps data as a column
-  bind_cols(raven_gps %>% st_drop_geometry) %>% 
+  bind_cols(raven_gps_sf %>% st_drop_geometry) %>% 
   rename(distance_ynp = ...1) %>% 
   #filtering to only distances > 0 (not inside the park)
   filter(distance_ynp > 0)
@@ -47,4 +47,28 @@ raven_gps_outside_ynp %>%
   write.csv("data/clean/mar_gps_outside_ynp.csv")
 
 
+# data sets to map based on model covariates -----------------
 
+#reading in commute data
+commute_df <- read_csv("data/clean/commute_data.csv") %>% 
+  #selecting useful columns
+  dplyr::select(raven_id, date, terr_bin, hunt_bin, dist2nentrance, temp_max)
+
+
+#raven GPS data with covariates to map by
+read_csv("data/clean/all_raven_gps_clean29.csv") %>% 
+  clean_names() %>% 
+  #only useful columns
+  dplyr::select(individual_local_identifier, utm_easting, utm_northing, study_local_timestamp) %>%
+  #extracting date
+  mutate(date = as.Date(study_local_timestamp)) %>% 
+  dplyr::select(-study_local_timestamp) %>% 
+  #adding commute data to GPS points
+  left_join(commute_df, by = join_by(individual_local_identifier == raven_id, date)) %>% 
+  #only complete rows
+  na.omit %>%
+  #only winter points
+  filter(month(date) %in% c(10:12, 1:3)) %>% 
+  write.csv("data/clean/raven_gps_covariates.csv")
+
+  
