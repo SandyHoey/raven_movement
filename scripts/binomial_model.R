40e887f974efa2244560626fb0b9b0e6f319ab2d
Cameron Ho <ho.cameron98@gmail.com>
2025-11-25 10:29:13 -0700

    moved to RF predictive model for kill covariates

--- scripts/binomial_model.R
+++ scripts/binomial_model.R
@@ -4,41 +4,32 @@ library(dplyr)
 
 ## dataset for part 1 of conditional model
 ws_model_data <- readr::read_csv("data/clean/commute_data.csv") %>% 
-  
   #restricting to only winter study months
   filter((paste(month, day, sep = "-") >= "11-15" &
             paste(month, day, sep = "-") <= "12-15") |
           (paste(month, day, sep = "-") >= "3-1" &
             paste(month, day, sep = "-") <= "3-30")) %>% 
-  
   #removing days when there is less than 5 GPS point
   #unless the result is Jardine
   filter(!(n_point < 5 & terr_bin == F)) %>% 
-  
   #only columns used in model
-  dplyr::select(terr_bin, raven_id, active_kill, bms_window_1, hunt_season, take_high_low,
-                avg_terr_kill_density, dist2nentrance, study_period, temp_max, 
-                snow_depth, prop_group_left_terr) %>% 
-  
+  dplyr::select(terr_bin, raven_id, rf_active_kill, bms_window_1, hunt_season, 
+                take_high_low, rf_avg_terr_kill_density, dist2nentrance, 
+                study_period, temp_max, snow_depth, prop_group_left_terr) %>% 
   #making sure rows are complete
   filter(complete.cases(.)) 
   
 
 ## dataset for part 2 of conditional model
 hunt_model_data <- readr::read_csv("data/clean/commute_data.csv") %>%
-
   #only have days ravens decided to leave territory
   filter(terr_bin == 1) %>% 
-  
   #removing days when there is less than 5 GPS point
   #unless the result is Jardine
   filter(!(n_point < 5 & hunt_bin == F)) %>% 
-  
-  
   #only columns used in model
-  dplyr::select(hunt_bin, raven_id, active_kill, bms_window_1, hunt_season, take_high_low,
-                avg_terr_kill_density, dist2nentrance, study_period, temp_max, 
-                snow_depth, prop_group_visit_hunt) %>% 
+  dplyr::select(hunt_bin, raven_id, bms_window_1, hunt_season, take_high_low,
+                dist2nentrance, study_period, temp_max, snow_depth, prop_group_visit_hunt) %>% 
   
   #making sure rows are complete
   filter(complete.cases(.)) 
@@ -76,7 +67,7 @@ cntrl <- glmerControl(optimizer = "bobyqa", tol = 1e-4, optCtrl=list(maxfun=1000
 
 #model with biomass number
 #I changed active_kill to only within 1 day of wolves leaving and that made a big difference
-mod_terr_bms1 <- glmer(terr_bin ~ (1|raven_id) + active_kill * scale(bms_window_1) + scale(avg_terr_kill_density) + 
+mod_terr_bms1 <- glmer(terr_bin ~ (1|raven_id) + rf_active_kill * scale(bms_window_1) + scale(rf_avg_terr_kill_density) + 
                          scale(dist2nentrance) + study_period + scale(temp_max) + scale(snow_depth) + scale(prop_group_left_terr),
                        data = ws_model_data,
                        family = "binomial",
@@ -87,7 +78,7 @@ summary(mod_terr_bms1)
 
 #model with hunting season (changes result for active_kill)
 #including the interaction effect messes with active_kill because of high error with interaction term
-mod_terr_hseason <- glmer(terr_bin ~ (1|raven_id) + active_kill * hunt_season + scale(avg_terr_kill_density) + 
+mod_terr_hseason <- glmer(terr_bin ~ (1|raven_id) + rf_active_kill * hunt_season + scale(rf_avg_terr_kill_density) + 
                             scale(dist2nentrance) + study_period + scale(temp_max) + scale(snow_depth) + scale(prop_group_left_terr),
                          data = ws_model_data,
                          family = "binomial",
@@ -97,7 +88,7 @@ summary(mod_terr_hseason)
 
 
 #model with categorical high/low hunt (no changes)
-mod_terr_hl <- glmer(terr_bin ~ (1|raven_id) + active_kill * take_high_low + scale(avg_terr_kill_density) + 
+mod_terr_hl <- glmer(terr_bin ~ (1|raven_id) + rf_active_kill * take_high_low + scale(rf_avg_terr_kill_density) + 
                             scale(dist2nentrance) + study_period + scale(temp_max) + scale(snow_depth) + scale(prop_group_left_terr),
                      data = ws_model_data,
                      family = "binomial",
