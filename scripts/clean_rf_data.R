902992df0d25706b3296b983af20ef6126972b8b
Cameron Ho <ho.cameron98@gmail.com>
2026-01-14 12:34:30 -0800

    cleaning comments

--- scripts/clean_rf_data.R
+++ scripts/clean_rf_data.R
@@ -1,21 +1,21 @@
-#takes the random forest predicted wolf kills for the winter (Nov, Dec, Mar) and minimizes it to a usable format for calculating covariates
+# takes the random forest predicted wolf kills for the winter (Nov, Dec, Mar) and minimizes it to a usable format for calculating covariates
 
 library(dplyr)
 
-#reading in RF input cluster data
-#using this to find the end time for each kill
+# reading in RF input cluster data
+# using this to find the end time for each kill
 cluster_data_rf <- readr::read_rds("data/raw/cluster_summaries_wolves_winter.rds") %>% 
   janitor::clean_names() %>% 
-  #only relevant columns
+  # only relevant columns
   dplyr::select(wolf_id, clus_start, clus_end, 
                 centroid_easting, centroid_northing)
 
-#reading in RF predicted wolf kills (only has winter study periods)
+# reading in RF predicted wolf kills (only has winter study periods)
 kill_data_rf <- readr::read_rds("data/raw/mergedkills_wolf_winter_RF_spec95.rds") %>% 
   janitor::clean_names() %>% 
-  #removing t2 column
+  # removing t2 column
   dplyr::select(-t2) %>% 
-  #renaming coordinate columns
+  # renaming coordinate columns
   rename(easting = x,
          northing = y) %>% 
   left_join(cluster_data_rf, 
@@ -23,15 +23,15 @@ kill_data_rf <- readr::read_rds("data/raw/mergedkills_wolf_winter_RF_spec95.rds"
                          northing == centroid_northing,
                          wolf_id,
                          kill_start_date == clus_start)) %>%
-  #rename datetime
+  # rename datetime
   rename(kill_start_dt = kill_start_date,
          kill_end_dt = clus_end) %>% 
-  #adding dedicated date column
+  # adding dedicated date column
   mutate(kill_start_date = as.Date(kill_start_dt),
          kill_end_date = as.Date(kill_end_dt)) %>% 
-  #placing date columns together
+  # placing date columns together
   relocate(kill_start_dt, .before = kill_end_dt) %>% 
-  #pulling a single row for each kill
+  # pulling a single row for each kill
   group_by(kill_id) %>% 
   slice(1) %>% 
   ungroup
