a67bef8f833ca8629d36e0d8f942d6c4c7146c4c
Cameron Ho <ho.cameron98@gmail.com>
2026-01-28 14:01:40 -0800

    moved to single model with both hunting covariates

--- scripts/binomial_model.R
+++ scripts/binomial_model.R
@@ -99,59 +99,74 @@ mod_terr_hseason <- glmer(terr_bin ~ (1|raven_id) + rf_active_kill + hunt_season
 summary(mod_terr_hseason)
 
 
-# model with raw hunting take (no adjustment for varying weights)
-# hunting covariates still not significant
-mod_terr_take <- glmer(terr_bin ~ (1|raven_id) + rf_active_kill * final_take + rf_avg_terr_kill_density + 
-                         dist2nentrance + study_period + temp_max + snow_depth + prop_group_left_terr,
-                       data = ws_model_data,
-                       family = "binomial",
-                       nAGQ = 40,
-                       control = cntrl)
-summary(mod_terr_take)
+# model with all the hunting covariates
+mod_terr <- glmer(terr_bin ~ (1|raven_id) + rf_active_kill * final_take_bms1 + hunt_season + rf_avg_terr_kill_density + 
+                    dist2nentrance + study_period * temp_max + snow_depth + prop_group_left_terr,
+                  data = ws_model_data,
+                  family = "binomial",
+                  nAGQ = 40,
+                  control = cntrl)
+summary(mod_terr)
 
 AIC(mod_terr_bms)
 AIC(mod_terr_hseason) #best
-AIC(mod_terr_take)
+AIC(mod_terr)
 
 
-# parameter confidence intervals bootstrap -------------------------------
-
-# bootstrapping parameter values from model simulations
-boot_terr_bms <- boot_param_CI(nsim = 50, model = mod_terr_bms, data = ws_model_data)
-
-# view effect plot
-boot_terr_bms[[3]]
-
+# bootstrap -------------------------------
 
 # bootstrapping parameter values from model simulations
-boot_terr_season <- boot_param_CI(nsim = 50, model = mod_terr_hseason, data = ws_model_data)
+boot_terr <- boot_param_CI(nsim = 3, model = mod_terr, data = ws_model_data,
+                           newData = expand.grid(rf_active_kill = c(TRUE, FALSE),
+                                                 hunt_season = c(TRUE, FALSE),
+                                                 final_take_bms1 = 0,
+                                                 rf_avg_terr_kill_density = 0,
+                                                 dist2nentrance = 0,
+                                                 study_period = "early",
+                                                 temp_max = 0,
+                                                 snow_depth = 0,
+                                                 prop_group_left_terr = 0))
 
 # view effect plot
-boot_terr_season[[3]]
+boot_terr[[3]]
 
+
+# plotting predictions for wolf kills and hunting season
+(terr_plot <- boot_terr[[4]] %>% 
+    # plotting
+    ggplot(aes(x = rf_active_kill, y = mean, col = rf_active_kill,
+               ymin = lower, ymax = upper)) +
+    geom_point() +
+    facet_wrap(~hunt_season, 
+               labeller = labeller(hunt_season = c("FALSE" = "No Hunting", "TRUE" = "Hunting"))) +
+    geom_errorbar(width = .1) +
+    labs(title = "Leaving territory",
+         x = "Active wolf kill",
+         y = "Predicted Probability"))
 
 # prediction bootstrap -----------------------------------------------
 
 # model with biomass number
-nsim = 10
+nsim = 3
 terr_bms_newData <- expand.grid(rf_active_kill = c(TRUE, FALSE),
-                                final_take_bms1 = seq(-2, 2, 0.1),
+                                hunt_season = c(TRUE, FALSE),
+                                final_take_bms1 = 0,
                                 rf_avg_terr_kill_density = 0,
                                 dist2nentrance = 0,
-                                study_period = "late",
+                                study_period = "early",
                                 temp_max = 0,
                                 snow_depth = 0,
                                 prop_group_left_terr = 0)
 # creating a data frame to put predicted values form each iteration
 terr_bms_pv <- matrix(NA, nrow = nsim,
-                  ncol = nrow(terr_bms_newData))
+                      ncol = nrow(terr_bms_newData))
 # getting the sd for the random effect
-terr_bms_RE <- as.data.frame(VarCorr(mod_terr_bms))$sdcor
+terr_bms_RE <- as.data.frame(VarCorr(mod_terr))$sdcor
 
 
 for(j in 1:nsim){
-  sim_data <- ws_model_data %>% mutate(y = unlist(simulate(mod_terr_bms)))
-  sim_model <- update(mod_terr_bms, y ~ ., data = sim_data)
+  sim_data <- ws_model_data %>% mutate(y = unlist(simulate(mod_terr)))
+  sim_model <- update(mod_terr, y ~ ., data = sim_data)
   
   # then predict from the updated model if the bootstrap sample converged 
   if(is.null(summary(sim_model)$optinfo$conv$lme4$messages) == TRUE){
@@ -167,10 +182,11 @@ terr_bms_newData$upper <- apply(terr_bms_pv, 2, function(x)quantile(x,p = 0.975,
 # plotting predictions for the wolf kills and hunter biomass
 (terr_bms_plot <- terr_bms_newData %>% 
     # plotting
-    ggplot(aes(x = final_take_bms1, y = mean, col = rf_active_kill,
+    ggplot(aes(x = rf_active_kill, y = mean, col = rf_active_kill,
                ymin = lower, ymax = upper)) +
-    geom_line() +
-    # geom_errorbar(width = .1) +
+    geom_point() +
+    facet_wrap(~hunt_season) +
+    geom_errorbar(width = .1) +
     labs(title = "Leaving territory",
          x = "Scaled available hunter biomass",
          y = "Predicted Probability")) +
@@ -180,16 +196,16 @@ terr_bms_newData$upper <- apply(terr_bms_pv, 2, function(x)quantile(x,p = 0.975,
 # model with hunting season
 nsim = 10
 terr_hseason_newData <- expand.grid(rf_active_kill = c(TRUE, FALSE),
-                                hunt_season = c(TRUE, FALSE),
-                                rf_avg_terr_kill_density = 0,
-                                dist2nentrance = 0,
-                                study_period = "late",
-                                temp_max = 0,
-                                snow_depth = 0,
-                                prop_group_left_terr = 0)
+                                    hunt_season = c(TRUE, FALSE),
+                                    rf_avg_terr_kill_density = 0,
+                                    dist2nentrance = 0,
+                                    study_period = "late",
+                                    temp_max = 0,
+                                    snow_depth = 0,
+                                    prop_group_left_terr = 0)
 # creating a data frame to put predicted values form each iteration
 terr_hseason_pv <- matrix(NA, nrow = nsim,
-                      ncol = nrow(terr_hseason_newData))
+                          ncol = nrow(terr_hseason_newData))
 # getting the sd for the random effect
 terr_hseason_RE <- as.data.frame(VarCorr(mod_terr_hseason))$sdcor
 
@@ -231,9 +247,8 @@ terr_hseason_newData$upper <- apply(terr_hseason_pv, 2, function(x)quantile(x,p 
 # 1 = visited hunting
 # 0 = visited other place
 
-
 # model with biomass number
-mod_hunt_bms <- glmer(hunt_bin ~ (1|raven_id) + visit_kill + final_take_bms1 + dist2nentrance + 
+mod_hunt_bms <- glmer(hunt_bin ~ (1|raven_id) + visit_kill * final_take_bms1 + dist2nentrance + 
                         prop_group_visit_hunt + temp_max + snow_depth,
                       data = hunt_model_data,
                       family = "binomial",
@@ -243,7 +258,7 @@ summary(mod_hunt_bms)
 
 
 # model with hunting season (changes study period, p value and effect direction)
-mod_hunt_hseason <- glmer(hunt_bin ~ (1|raven_id) + visit_kill + hunt_season + dist2nentrance + 
+mod_hunt_hseason <- glmer(hunt_bin ~ (1|raven_id) + visit_kill * hunt_season + dist2nentrance + 
                             prop_group_visit_hunt + temp_max + snow_depth,
                           data = hunt_model_data,
                           family = "binomial",
@@ -253,32 +268,48 @@ summary(mod_hunt_hseason)
 
 
 # model with raw hunting take (no adjustment for varying weights)
-mod_hunt_take <- glmer(hunt_bin ~ (1|raven_id) + final_take + visit_kill + dist2nentrance + 
-                         prop_group_visit_hunt + temp_max + snow_depth,
-                       data = hunt_model_data,
-                       family = "binomial",                       
-                       nAGQ = 40,
-                       control = cntrl)
-summary(mod_hunt_take)
+mod_hunt <- glmer(hunt_bin ~ (1|raven_id) + visit_kill * final_take_bms1 + hunt_season + dist2nentrance + 
+                    prop_group_visit_hunt + temp_max + snow_depth,
+                  data = hunt_model_data,
+                  family = "binomial",
+                  nAGQ = 40,
+                  control = cntrl)
+summary(mod_hunt)
 
 AIC(mod_hunt_bms)
-AIC(mod_hunt_hseason) #best
-AIC(mod_hunt_take)
+AIC(mod_hunt_hseason) 
+AIC(mod_hunt) #best
 
 
-# parameter confidence intervals bootstrap -------------------------------
-
-# bootstrapping parameter values from model simulations
-boot_hunt_bms <- boot_param_CI(nsim = 50, model = mod_hunt_bms, data = hunt_model_data)
-
-# view effect plot
-boot_hunt_bms[[3]]
+# bootstrap -------------------------------
 
 # bootstrapping parameter values from model simulations
-boot_hunt_season <- boot_param_CI(nsim = 50, model = mod_hunt_hseason, data = hunt_model_data)
+boot_hunt <- boot_param_CI(nsim = 5, model = mod_hunt, data = hunt_model_data, 
+                           newData = expand.grid(visit_kill = c(TRUE, FALSE),
+                                                 hunt_season = c(TRUE, FALSE),
+                                                 final_take_bms1 = 0,
+                                                 dist2nentrance = 0,
+                                                 temp_max = 0,
+                                                 snow_depth = 0,
+                                                 prop_group_visit_hunt = 0))
 
-# view effect plot
-boot_hunt_season[[3]]
+# parameter estimates
+boot_hunt[[3]]
+
+
+# plotting predictions for wolf kills and hunting season
+(hunt_plot <- boot_hunt[[4]] %>% 
+  # plotting
+  ggplot(aes(x = visit_kill, y = mean, col = visit_kill,
+             ymin = lower, ymax = upper)) +
+  geom_point() +
+  facet_wrap(~hunt_season, 
+             labeller = labeller(hunt_season = c("FALSE" = "No Hunting", "TRUE" = "Hunting"))) +
+  geom_errorbar(width = .1) +
+  labs(title = "Visiting hunting region",
+       x = "Found Kill",
+       y = "Predicted Probability"))
+
 
 # prediction bootstrap -----------------------------------------------
 
@@ -326,7 +357,7 @@ hunt_bms_newData$upper <- apply(hunt_bms_pv, 2, function(x)quantile(x,p = 0.975,
 
 
 # model with hunting season
-nsim = 10
+nsim = 500
 hunt_hseason_newData <- expand.grid(visit_kill = c(TRUE, FALSE),
                                     hunt_season = c(TRUE, FALSE),
                                     dist2nentrance = 0,
@@ -358,11 +389,13 @@ hunt_hseason_newData$upper <- apply(hunt_hseason_pv, 2, function(x)quantile(x,p 
 # plotting predictions for wolf kills and hunting season
 (hunt_hseason_plot <- hunt_hseason_newData %>% 
     # plotting
-    ggplot(aes(x = rf_active_kill, y = mean, col = rf_active_kill,
+    ggplot(aes(x = visit_kill, y = mean, col = visit_kill,
                ymin = lower, ymax = upper)) +
     geom_point() +
-    facet_wrap(~hunt_season) +
+    facet_wrap(~hunt_season, 
+               labeller = labeller(hunt_season = c("FALSE" = "No Hunting", "TRUE" = "Hunting"))) +
     geom_errorbar(width = .1) +
     labs(title = "Visiting hunting region",
-         x = "Hunting season",
+         x = "Found Kill",
          y = "Predicted Probability"))
+
