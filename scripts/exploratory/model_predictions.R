c0bf16ce835460dcb9b3faf53f7e5e0a1a848405
Cameron Ho <ho.cameron98@gmail.com>
2026-01-28 18:30:45 -0800

    updated to single model predictions

--- scripts/exploratory/model_predictions.R
+++ scripts/exploratory/model_predictions.R
@@ -4,41 +4,42 @@ scale_seq <- seq(-2, 2, .005)
 # hunting biomass and finding kill outside territory
 # red = found a kill
 plot(x = scale_seq,
-     y = plogis(c(0.65996 - 1.18325 + -0.03807 * scale_seq)),
+     y = plogis(c(-0.16767 - 1.26290 - 0.04314 * scale_seq)),
      col = "red", ylim = c(0, 1))
 lines(x = scale_seq,
-      y = plogis(c(0.65996 + -0.03807 * scale_seq)),
+      y = plogis(c(-0.16767 - 0.04314 * scale_seq)),
       col = "blue")
 
 
 # hunting season and finding a kill outside territory
 plot(x = 1:4, 
-     y = plogis(c(-0.10061 - 1.15507 + 0.83231,
-                  -0.10061 - 1.15507,
-                  -0.10061 + 0.83231,
-                  -0.10061)),
-     main = "Active kill, Hunt season")
+     y = plogis(c(-0.16767 - 1.26290 + 0.90412,
+                  -0.16767 - 1.26290,
+                  -0.16767 + 0.90412,
+                  -0.16767)),
+     main = "Active kill, Hunt season",
+     ylim = c(0,1))
 
 
 # finding kill outside territory + distance from hunting
 # red = found a kill
 plot(x = scale_seq,
-     y = plogis(c(-0.10061 - 1.5507 -1.70818 * scale_seq)),
+     y = plogis(c(-0.16767 -1.26290 -1.71212 * scale_seq)),
      col = "red", ylim = c(0, 1))
 lines(x = scale_seq,
-      y = plogis(c(-0.10061 - 1.5507 + 0.27617 - (1.70818 + 0.43359) * scale_seq)),
+      y = plogis(c(-0.16767 -1.26290 + 0.29596 - 1.71212 * scale_seq)),
       col = "red", lty = 2)
 lines(x = scale_seq,
-      y = plogis(c(-0.10061 - 1.5507 - 0.27617 - (1.70818 - 0.43359) * scale_seq)),
+      y = plogis(c(-0.16767 -1.26290 - 0.29596 - 1.71212 * scale_seq)),
       col = "red", lty = 2)
 lines(x = scale_seq,
-      y = plogis(c(-0.10061 - 1.70818 * scale_seq)),
+      y = plogis(c(-0.16767 - 1.71212 * scale_seq)),
       col = "blue")
 lines(x = scale_seq,
-      y = plogis(c(-0.10061 - (1.70818 + 0.43359) * scale_seq)),
+      y = plogis(c(-0.16767 - (1.71212 + 0.43332) * scale_seq)),
       col = "blue", lty = 2)
 lines(x = scale_seq,
-      y = plogis(c(-0.10061 - (1.70818 - 0.43359) * scale_seq)),
+      y = plogis(c(-0.16767 - (1.71212 - 0.43332) * scale_seq)),
       col = "blue", lty = 2)
 
 
