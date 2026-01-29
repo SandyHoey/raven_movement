42a5db24a6cbd370f8e229ec0afc5b1383be2dab
Cameron Ho <ho.cameron98@gmail.com>
2026-01-29 10:22:28 -0800

    added conditional part 1

--- scripts/exploratory/model_predictions.R
+++ scripts/exploratory/model_predictions.R
@@ -1,5 +1,44 @@
 scale_seq <- seq(-2, 2, .005)
 
+# PART 1
+# hunting biomass and active kill in territory
+# red = active kill
+plot(x = scale_seq,
+     y = plogis(c(2.09939 - 0.86381 - 0.05167 * scale_seq)),
+     col = "red", ylim = c(0, 1))
+lines(x = scale_seq,
+      y = plogis(c(2.09939 - 0.05167 * scale_seq)),
+      col = "blue")
+
+
+# hunting season and active kill in territory
+plot(x = 1:4, 
+     y = plogis(c(2.09939 - 0.86381 + 0.88014,
+                  2.09939 - 0.86381,
+                  2.09939 + 0.88014,
+                  2.09939)),
+     main = "Active kill, Hunt season",
+     ylim = c(0,1))
+
+
+# finding kill outside territory and study period
+# red = found a kill
+plot(x = 1:4,
+     y = plogis(c(2.09939 - 1.26290,
+                  2.09939 - 1.26290 - 0.54742,
+                  2.09939,
+                  2.09939 - 0.54742)),
+     main = "Active kill, Study period",
+     ylim = c(0, 1))
+text(x = 1:4,
+     y = 0.5,
+     labels = c("early", "late", "early", "late"))
+text(x = c(1.5, 3.5),
+     y = 0.3,
+     labels = c("Active Kill", "No Kill"))
+
+
+
 # PART 2
 # hunting biomass and finding kill outside territory
 # red = found a kill
