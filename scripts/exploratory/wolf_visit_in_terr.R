a53b3fe8bddd514e9dea8eb7e0a7f42d6023a697
Cameron Ho <ho.cameron98@gmail.com>
2026-02-10 18:10:59 -0800

    fixed and made summary stats

--- scripts/exploratory/wolf_visit_in_terr.R
+++ scripts/exploratory/wolf_visit_in_terr.R
@@ -1,17 +1,13 @@
 # janky post hoc investigation of how often raven had wolf kills on their territories that they visited
 # visit = GPS point within 500 m
 
-# NOT COMPLETE
-# got to complicated trying to add kill coordinates to raven GPS points
-# since there could be more than 1 carcass on a day
-
 in_terr_kill_df <- rf_in_terr_kill_list %>% 
   # adding column with list name to each df
   purrr::imap(~ mutate(.x, raven_id = .y)) %>% 
   # list to df
-  do.call("rbind", .)
-  # only useful columns
-  dplyr::select(raven_id, kill_number, easting, northing)
+  do.call("rbind", .) %>% 
+  # making the kill end dates match the availability
+  mutate(kill_end_date = kill_end_date + 1)
 
 
 commute_df_covariates <- readr::read_csv("data/clean/commute_data.csv") %>% 
@@ -30,7 +26,7 @@ commute_df_covariates <- readr::read_csv("data/clean/commute_data.csv") %>%
   # making sure rows are complete
   filter(complete.cases(.)) %>% 
   # only columns useful for this
-  dplyr::select(raven_id, date, rf_active_kill)
+  dplyr::select(raven_id, date, rf_active_kill, terr_bin)
 
 gps_raven <- readr::read_csv("data/clean/all_raven_gps_clean29.csv") %>% 
   janitor::clean_names() %>% 
@@ -43,6 +39,32 @@ gps_raven <- readr::read_csv("data/clean/all_raven_gps_clean29.csv") %>%
                                                 date)) %>% 
   # only days when wolf kill was available on terr
   filter(rf_active_kill == TRUE) %>% 
-  # adding kill location
-  left_join(in_terr_kill_df, by = join_by(individual_local_identifier = raven_id,
-                                          date))
+  # making column for date
+  mutate(date = as.Date(study_local_timestamp))
+
+
+
+
+kill_rows <- in_terr_kill_df %>%
+  inner_join(gps_raven, by = c("raven_id" = "individual_local_identifier")) %>%
+  filter(date >= kill_start_date, date <= kill_end_date) %>%
+  mutate(dist_m = sqrt((utm_easting  - easting)^2 +
+                         (utm_northing - northing)^2)) %>%
+  group_by(kill_id, date) %>% 
+  filter(dist_m == min(dist_m)) %>% 
+  ungroup %>% 
+  mutate(visit_500 = if_else(dist_m < 500, TRUE, FALSE),
+         visit_1000 = if_else(dist_m < 1000, TRUE, FALSE)) %>% 
+  dplyr::select(kill_id, raven_id, visit_500, visit_1000, terr_bin, date) %>% 
+  arrange(raven_id, kill_id, date)
+  
+table(kill_rows$visit_500)
+table(kill_rows$visit_1000)
+# how many times did ravens never visit the kill
+kill_rows %>% 
+  group_by(kill_id) %>% 
+  summarize(found = any(visit_500 == TRUE)) %>% 
+  ungroup %>% 
+  pull(found) %>% 
+  sum
+8/25
