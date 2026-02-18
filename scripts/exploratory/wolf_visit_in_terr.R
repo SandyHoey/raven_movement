bb34d6c00fac6ddb4861ef5d2972e7c65702d3c3
Cameron Ho <ho.cameron98@gmail.com>
2026-02-18 12:34:00 -0800

    modeled simple relations for discussion

--- scripts/exploratory/wolf_visit_in_terr.R
+++ scripts/exploratory/wolf_visit_in_terr.R
@@ -1,6 +1,8 @@
 # janky post hoc investigation of how often raven had wolf kills on their territories that they visited
 # visit = GPS point within 500 m
 
+library(dplyr)
+
 in_terr_kill_df <- rf_in_terr_kill_list %>% 
   # adding column with list name to each df
   purrr::imap(~ mutate(.x, raven_id = .y)) %>% 
@@ -28,6 +30,11 @@ commute_df_covariates <- readr::read_csv("data/clean/commute_data.csv") %>%
   # only columns useful for this
   dplyr::select(raven_id, date, rf_active_kill, terr_bin)
 
+
+# adding raven territory size
+source("scripts/home_range_mcp.R")
+
+# adding information about available kills to raven GPS points
 gps_raven <- readr::read_csv("data/clean/all_raven_gps_clean29.csv") %>% 
   janitor::clean_names() %>% 
   # only useful columns
@@ -37,25 +44,34 @@ gps_raven <- readr::read_csv("data/clean/all_raven_gps_clean29.csv") %>%
   # adding commute data to GPS points
   left_join(commute_df_covariates, by = join_by(individual_local_identifier == raven_id, 
                                                 date)) %>% 
+  # adding raven territory size
+  left_join(mcp90@data, by = join_by(individual_local_identifier == id)) %>% 
   # only days when wolf kill was available on terr
   filter(rf_active_kill == TRUE) %>% 
   # making column for date
   mutate(date = as.Date(study_local_timestamp))
 
 
-
-
+# IGNORE WARNING SINCE THERE ARE MULTIPLE GPS POINTS PER DAY
+# joining kill location with raven GPS points
 kill_rows <- in_terr_kill_df %>%
   inner_join(gps_raven, by = c("raven_id" = "individual_local_identifier")) %>%
+  # only keeping GPS points with active kill information
   filter(date >= kill_start_date, date <= kill_end_date) %>%
+  # calculating distance to active kills
   mutate(dist_m = sqrt((utm_easting  - easting)^2 +
                          (utm_northing - northing)^2)) %>%
+  # only keeping the closest GPS point per day
   group_by(kill_id, date) %>% 
   filter(dist_m == min(dist_m)) %>% 
   ungroup %>% 
+  # creating column with day since kill
+  mutate(delta_dod = as.numeric(date - kill_start_date)) %>% 
+  # creating column to define a visited kill
   mutate(visit_500 = if_else(dist_m < 500, TRUE, FALSE),
          visit_1000 = if_else(dist_m < 1000, TRUE, FALSE)) %>% 
-  dplyr::select(kill_id, raven_id, visit_500, visit_1000, terr_bin, date) %>% 
+  # keeping only relevant columns
+  dplyr::select(kill_id, raven_id, visit_500, visit_1000, terr_bin, date, delta_dod, area) %>% 
   arrange(raven_id, kill_id, date)
   
 table(kill_rows$visit_500)
@@ -68,3 +84,32 @@ kill_rows %>%
   pull(found) %>% 
   sum
 8/25
+
+
+
+# doing some testing on probabilities of visiting a kill ------------------
+library(lme4)
+
+# based on size of territory
+terr_size_500 <- glm(visit_500 ~ area, 
+                     data = kill_rows, 
+                     family = "binomial")
+summary(terr_size_500)
+
+terr_size_1000 <- glm(visit_1000 ~ area, 
+                      data = kill_rows, 
+                      family = "binomial")
+summary(terr_size_1000)
+
+
+
+# based on days since the kill
+delta_dod_500 <- glm(visit_500 ~ delta_dod, 
+                     data = kill_rows, 
+                     family = "binomial")
+summary(delta_dod_500)
+
+delta_dod_1000 <- glm(visit_1000 ~ delta_dod, 
+                      data = kill_rows, 
+                      family = "binomial")
+summary(delta_dod_1000)
