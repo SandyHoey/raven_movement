2db48794c9425b51bb340281e67ca87eeb4a44c6
Cameron Ho <ho.cameron98@gmail.com>
2026-04-02 17:10:31 -0700

    updated coefficients

--- scripts/exploratory/model_predictions.R
+++ scripts/exploratory/model_predictions.R
@@ -1,41 +1,28 @@
 scale_seq <- seq(-2, 2, .005)
 
 # PART 1
-# hunting biomass and active kill in territory
-# red = active kill
-plot(x = scale_seq,
-     y = plogis(c(3.18873 - 0.79651 - 0.05167 * scale_seq)),
-     col = "red", ylim = c(0, 1))
-lines(x = scale_seq,
-      y = plogis(c(3.18873 - 0.05167 * scale_seq)),
-      col = "blue")
-
-
 # hunting season and active kill in territory
 plot(x = 1:4, 
-     y = plogis(c(3.18873 - 0.79651 - 0.21529,
-                  3.18873 - 0.79651,
-                  3.18873 - 0.21529,
-                  3.18873)),
+     y = plogis(c(3.463227 - 0.782575 - 0.128697,
+                  3.463227 - 0.782575,
+                  3.463227 - 0.128697,
+                  3.463227)),
      main = "Active kill, Hunt season",
      ylim = c(0,1))
 
 
-# active kill and study period
-# red = found a kill
-plot(x = 1:4,
-     y = plogis(c(3.18873 - 0.79651,
-                  3.18873 - 0.79651 - 0.4603,
-                  3.18873,
-                  3.18873 - 0.4603)),
-     main = "Active kill, Study period",
-     ylim = c(0, 1))
-text(x = 1:4,
-     y = 0.5,
-     labels = c("early", "late", "early", "late"))
-text(x = c(1.5, 3.5),
-     y = 0.3,
-     labels = c("Active Kill", "No Kill"))
+# examining interaction effect study_period*snow
+# red = Late winter
+# green = Early winter
+plot(x = scale_seq,
+     y = plogis(3.463227 - 0.782575 + (0.687682 * scale_seq) - 0.554340),
+     main = "Stdy period * snow",
+     xlab = "snow",
+     ylim = c(0, 1),
+     col = 2, lwd = 0.1)
+lines(x = scale_seq, 
+      y = plogis(3.463227 + (0.687682 * scale_seq) - 0.554340),
+      col = 3, lwd = 2)
 
 
 
