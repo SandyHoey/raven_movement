9148dcb03b686d5a0ae47f50a09490d11dfe550c
Cameron Ho <ho.cameron98@gmail.com>
2026-04-16 11:48:14 -0700

    added early/late winter predictions

--- scripts/exploratory/model_predictions.R
+++ scripts/exploratory/model_predictions.R
@@ -2,13 +2,30 @@ scale_seq <- seq(-2, 2, .005)
 
 # PART 1
 # hunting season and active kill in territory
-plot(x = 1:4, 
-     y = plogis(c(3.464512 - 0.666247 - 0.137657,
-                  3.464512 - 0.666247,
-                  3.464512 - 0.137657,
-                  3.464512)),
-     main = "Active kill, Hunt season",
-     ylim = c(0,1))
+# early winter
+plogis(c(3.464512 - 0.666247 - 0.137657,
+         3.464512 - 0.666247,
+         3.464512 - 0.137657,
+         3.464512))
+# late winter
+plogis(c(3.464512 - 0.666247 - 0.137657 - 0.7935797,
+         3.464512 - 0.666247 - 0.7935797,
+         3.464512 - 0.137657 - 0.7935797,
+         3.464512 - 0.7935797))
+
+
+# hunting season and visit kill (500m) in terr
+# early winter
+plogis(c(3.4663164 - 1.7007274 - 0.1326784,
+         3.4663164 - 1.7007274,
+         3.4663164 - 0.1326784,
+         3.4663164))
+# late winter
+plogis(c(3.4663164 - 1.7007274 - 0.1326784 - 0.7935797,
+         3.4663164 - 1.7007274 - 0.7935797,
+         3.4663164 - 0.1326784 - 0.7935797,
+         3.4663164 - 0.7935797))
+
 
 
 # examining interaction effect study_period*snow
@@ -38,13 +55,10 @@ lines(x = scale_seq,
 
 
 # hunting season and finding a kill outside territory
-plot(x = 1:4, 
-     y = plogis(c(0.22332 - 2.21482 + 0.82175, # kill, hunt
-                  0.22332 - 2.21482, # kill, no hunt
-                  0.22332 + 0.82175, # no kill, hunt 
-                  0.22332)), # no kill, no hunt
-     main = "Active kill, Hunt season",
-     ylim = c(0,1))
+plogis(c(0.22332 - 2.21482 + 0.82175, # kill, hunt
+         0.22332 - 2.21482, # kill, no hunt
+         0.22332 + 0.82175, # no kill, hunt 
+         0.22332)) # no kill, no hunt
 
 
 # finding kill outside territory + distance from hunting
